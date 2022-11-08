472f309ef2958628e213cc82681e919d215e74d2
Nikolay-Davydov <davidovns70@gmail.com>
2022-11-08 12:13:12 +0100

    update nicknames.js const patternTwo

--- src/js/nicknames.js
+++ src/js/nicknames.js
@@ -1,7 +1,7 @@
 export default class Validator {
   validateUsername(username) {
     const patternOne = /[a-z]/i.test(username);
-    const patternTwo = /^[^\d_-\W]+[\w-]*[^\d_-\W]$/.test(username);
+    const patternTwo = /^[a-zA-Z]+[\w-]*[^\d_-\W]$/.test(username);
     const patternTree = /[\d]{3,}/.test(username);
     if ((username.length === 1 && patternOne) || (patternTwo && !patternTree)) {
       this.username = username;
